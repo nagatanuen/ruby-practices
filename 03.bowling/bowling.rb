38d6cd8cda2cd7b58f7c2c407848e1bd53fc356e
nagatanuen <tomohiro.tano@gmail.com>
2024-03-03 15:58:42 +0900

    自明なコメントを消した

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -10,10 +10,7 @@ class MyBowling
     total_score = 0
     @frames.each_with_index do |frame, i|
       total_score += frame.sum
-
-      # 10フレーム目はボーナスの加算がないので抜ける
       break if i == 9
-
       total_score += calc_strike_bonus(i) if strike?(frame)
       total_score += calc_spare_bonus(i) if spare?(frame)
     end
@@ -31,7 +28,6 @@ class MyBowling
   end
 
   def calc_strike_bonus(index)
-    # 次のフレームもストライクの場合は、次の次のフレームの1投目のスコアも加算する
     bonus = 0
     next_frame = @frames[index + 1]
     next_next_frame = @frames[index + 2]
@@ -50,11 +46,9 @@ class MyBowling
     next_frame[0]
   end
 
-  # 引数をパースして1投ごとの結果を得る
   def create_frames(text)
     text_array = text.split(',')
     scores = text_array.map { |s| s == 'X' ? 10 : s.to_i }
-    # 1投ごとの結果をフレームにセットする
     @frames = []
     10.times do |i|
       first = scores.shift
@@ -63,7 +57,6 @@ class MyBowling
         @frames[i] = [first, second]
       else
         second = scores.shift
-        # 10フレーム目1投目がストライクもしくは2投目がスペアだった場合、3投目が投げられる
         third = first == 10 || first + second == 10 ? scores.shift : 0
         @frames[i] = [first, second, third]
       end
