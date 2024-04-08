d2fcbd122922f5d77e45447cd4ea83c8f0c250d6
nagatanuen <tomohiro.tano@gmail.com>
2024-04-09 00:28:23 +0900

    lsコマンドで-lオプションが使えるようにした

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,15 +2,37 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
+
 class Ls
   CURRENT_PATH = '.'
   MAX_COLUMN = 3
+  FILE_TYPE = {
+    '01' => 'p',
+    '02' => 'c',
+    '04' => 'd',
+    '06' => 'b',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }.freeze
+  PERMISSION = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
 
   def initialize(params)
     @option = {}
     opt = OptionParser.new
     opt.on('-a') { @option[:all] = true }
     opt.on('-r') { @option[:reverse] = true }
+    opt.on('-l') { @option[:list] = true }
     opt.parse!(params)
     @path = params[0] || CURRENT_PATH
   end
@@ -18,7 +40,11 @@ class Ls
   def display
     return 'エラー：指定されたディレクトリが存在しません' unless FileTest.directory?(@path)
 
-    display_without_option(file_list)
+    if @option[:list]
+      display_with_list_option(file_list)
+    else
+      display_without_list_option(file_list)
+    end
   end
 
   private
@@ -33,7 +59,83 @@ class Ls
     @option[:reverse] ? files.reverse : files
   end
 
-  def display_without_option(files)
+  def display_with_list_option(files)
+    lines = create_line(files)
+    format_lines(lines)
+  end
+
+  def create_line(files)
+    lines = []
+
+    files.each do |file|
+      line = {}
+      fs = File.lstat("#{@path}/#{file}")
+      mode = fs.mode.to_s(8).rjust(6, '0')
+      type = file_type(mode.slice(0..1))
+      owner = permissions(mode.slice(3))
+      group = permissions(mode.slice(4))
+      other = permissions(mode.slice(5))
+
+      line[:permission] = "#{type}#{special_permissions(mode.slice(2), "#{owner}#{group}#{other}")}"
+      line[:nlink] = fs.nlink.to_s
+      line[:uname] = Etc.getpwuid(fs.uid).name
+      line[:gname] = Etc.getgrgid(fs.gid).name
+      line[:size] = fs.size.to_s
+      line[:updated_at] = format_date(fs.mtime.to_s)
+      line[:file] = fs.symlink? ? "#{file} -> #{File.readlink("#{@path}/#{file}")}" : file
+
+      lines << line
+    end
+    lines
+  end
+
+  def format_lines(lines)
+    output = []
+    nlink_width = calc_column_width(lines, column: :nlink)
+    uname_width = calc_column_width(lines, column: :uname)
+    gname_width = calc_column_width(lines, column: :gname)
+    size_width = calc_column_width(lines, column: :size)
+    updated_at_width = calc_column_width(lines, column: :updated_at)
+    lines.each do |l|
+      output << "#{l[:permission]} #{l[:nlink].rjust(nlink_width)} "\
+                "#{l[:uname].ljust(uname_width)} #{l[:gname].ljust(gname_width)} "\
+                "#{l[:size].rjust(size_width)} #{l[:updated_at].ljust(updated_at_width)} #{l[:file]}"
+    end
+    output
+  end
+
+  def format_date(date)
+    y = date.slice(0..3)
+    m = date.slice(5..6)
+    d = date.slice(8..9)
+    hh = date.slice(11..12)
+    mm = date.slice(14..15)
+    ss = date.slice(17..18)
+    t = Time.new(y, m, d, hh, mm, ss, '+09:00')
+    t.strftime('%b %e %H:%M')
+  end
+
+  def file_type(mode)
+    FILE_TYPE[mode]
+  end
+
+  def permissions(mode)
+    PERMISSION[mode]
+  end
+
+  def special_permissions(mode, permission)
+    case mode
+    when '1'
+      permission[8] = permission[8] == 'x' ? 't' : 'T'
+    when '2'
+      permission[5] = permission[5] == 'x' ? 's' : 'S'
+    when '4'
+      permission[2] = permission[2] == 'x' ? 's' : 'S'
+    end
+    permission
+  end
+
+  def display_without_list_option(files)
     max_row = calc_max_row(files)
     table = Array.new(max_row) { Array.new(MAX_COLUMN) }
     i = 0
@@ -65,8 +167,14 @@ class Ls
     end
   end
 
-  def calc_column_width(files)
-    files.max_by(&:length).length
+  def calc_column_width(list, column: nil)
+    if column
+      # debugger
+      target = list.max_by { |l| l[column].length }
+      target[column].length
+    else
+      list.max_by(&:length).length
+    end
   end
 end
 
